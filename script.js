4cc3f7de463f5aca2b89f924cbb53fb3662f10a2
wechu07 <wechuli017@gmail.com>
2022-07-19 22:51:19 +0300

    response js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
 
+const form = document.querySelector("[data-form]")
 const queryParamsContainer = document.querySelector("[data-query-params]")
 const requestHeadersContainer = document.querySelector("[data-request-headers]")
 const keyValueTemplate = document.querySelector("[data-key-value-template]")
+const responseHeadersContainer = document.querySelector("[data-response-headers]")
 
 document.querySelector("[data-add-query-param-btn]").addEventListener("click", () => {
     queryParamsContainer.append(createKeyValuePair())
@@ -24,14 +27,40 @@ const createKeyValuePair = () => {
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
 
-FormData.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
     e.preventDefault()
 
     axios({
-    url: document.querySelector('data-url').value,
-    method: document.querySelector('data-method').value,
+    url: document.querySelector("[data-url]").value,
+    method: document.querySelector("[data-method]").value,
     params: keyValuePairsToObjects(queryParamsContainer),
-    headers: keyValuePairsToObjects(requestHeadersContainer),
-    data,
+    headers: keyValuePairsToObjects(requestHeadersContainer)
+    }).then(response => {
+        document.querySelector("[data-response-section]")
+        .classList.remove("d-none")
+        // updateResponseDetails(response)
+        // updateResponseEditor(response.data)
+        updateResponseHeaders(response.headers)
+        console.log(response)
     })
 })
+
+function updateResponseHeaders (headers) {
+    responseHeadersContainer.innerHTML = "";
+    Object.entries(headers).forEach(([key, value]) => {
+            const keyElement = document.createElement('div')
+            keyElement.textContent = key
+            responseHeadersContainer.append(key)
+        })
+}
+
+function keyValuePairsToObjects(container)  {
+    const pairs = container.querySelectorAll('[data-key-value-pair]')
+    return [...pairs].reduce((data, pair) => {
+        const key = pair.querySelector('[data-key]').value
+        const value = pair.querySelector('[data-key]').value
+
+        if (key === '') return data //if empty string
+        return {...data, [key]: value} //add it into the existing data
+    }, {})
+}
